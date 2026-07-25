b660ad8546224c1d929a97e540aacfd71fb37406
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-07-25 21:55:32 +0100

    Echoing

--- api/views.py
+++ api/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+import json
 # Create your views here.
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"Message": "Hi there, This is my first Django API response"})
+    # request --> HttpRequest --> Django
+    # print(dirs(request))
+    # request.body
+
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers) # request.META ->
+    data['content_type'] = dict(request.content_type)
+    return JsonResponse(data)
