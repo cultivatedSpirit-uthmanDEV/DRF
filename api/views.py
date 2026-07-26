0739dc35782eaea83c11a409338015268415c116
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-07-26 21:23:51 +0100

    Django Rest Framework Modle Serilizers

--- api/views.py
+++ api/views.py
@@ -3,8 +3,12 @@ from django.http import JsonResponse
 import json
 from products.models import Product
 from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from products.serilizers import ProductSerializer
 # Create your views here.
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     """"
         # request --> HttpRequest --> Django
@@ -24,20 +28,35 @@ def api_home(request, *args, **kwargs):
     """
 
 
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
+    if instance:
        """ data['title'] = model_data.title
         data['content'] = model_data.content
-        data['price'] = model_data.price"""
-       data = model_to_dict(model_data, fields=['id', 'title'])
+        data['price'] = model_data.price
+        """
+         #data = model_to_dict(instance, fields=['id', 'title','price', 'sale_price'])
+       data = ProductSerializer(instance).data
 
         # model instance (model_Data)
         # turn a python dict
         # return JSON to my client
-    return JsonResponse(data)
+        #return JsonResponse(data)
+    return Response(data)
 
 
 
-
+"""
+Welcome to the Django Rest Framework
+Tools we are using
+Setup Python Virtual Environment, Install Req, and Start Django
+Creating a Python API Client
+Run Django Project
+Create your first API View
+Echo GET Data
+Django Model Instance as API Response
+"""
 "Next topic : DJango Model instance to dictionary"
+"Next topic : Rest Framwwork View and response"
+"Next topic : Django Rest Framework Modle Serilizers"
+"Next topic : Ingest Data with Django Rest Framework views"
