8cc237187181ba54951dfa4584c7b5d1fc878836
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-08-02 17:15:58 +0100

    Framwwork Generics CreateAPIView

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.product_create_view.as_view()),
     path('<int:pk>/', views.product_details_view.as_view())
+   
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,24 @@ from rest_framework import generics
 from .models import Product
 from products.serializers import ProductSerializer
 
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    def perform_create(self, serializer):
+        #serializer.save(user= self.request.user)
+        #print(serializer)
+        #serializer.save()
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content')
+        #or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+
+product_create_view = ProductCreateAPIView
+
 class ProductDetailAPIViews(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
