1664e10ee578f18b718937a932aed508f7ff7d41
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-08-07 21:52:21 +0100

    Mixins and a Generics APIview

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from .models import Product
 from products.serializers import ProductSerializer
@@ -55,6 +55,36 @@ class ProductDestroyAPIViews(generics.DestroyAPIView):
         #      instance
          #     super().perform_destroy()
 product_delete_view = ProductDestroyAPIViews.as_view()
+
+class ProductMixinView(
+      mixins.ListModelMixin,
+      mixins.RetrieveModelMixin,
+      mixins.CreateModelMixin,
+      generics.GenericAPIView
+):
+      queryset = Product.objects.all()
+      serializer_class = ProductSerializer
+      lookup_field = "pk"
+      def get(self, request, *args, **kwargs):
+            pk = kwargs.get("pk")
+            if pk is not None:
+                  return self.retrieve(request, *args, **kwargs)
+            return self.list(request, *args, **kwargs)
+      def post(self, request, *args, **kwargs):
+            return self.create(request, *args, **kwargs)
+
+      def perform_create(self, serializer):
+                  #serializer.save(user= self.request.user)
+                  #print(serializer)
+                  #serializer.save()
+                  title = serializer.validated_data.get('title')
+                  content = serializer.validated_data.get('content')
+                  #or None
+                  if content is None:
+                      content = "This is a single view doing cool stuff"
+                  serializer.save(content=content)
+product_mixin_view = ProductMixinView
+
 
 
 @api_view(["POST", "GET"])
