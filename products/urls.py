ddf11d001a7fabc61ccb712ed2d7cc8b12d8a355
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-08-10 03:45:15 +0100

    User & Group permissions with DjangoModelPermissions

--- products/urls.py
+++ products/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_mixin_view.as_view()),
+    path('', views.product_list_create_view.as_view()),
     path('<int:pk>/delete/', views.product_delete_view),
     path('<int:pk>/update/', views.product_update_view),
-    path('<int:pk>/', views.product_mixin_view.as_view())
+    path('<int:pk>/', views.product_details_view.as_view())
     
     
    
